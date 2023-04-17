f529c467cebab788a09dd3dd42e44e4a98711fec
kevin11jeferson <kevin11jeferson@gmail.com>
2023-04-17 01:41:17 -0500

    KEVIN

--- js/carro.js
+++ js/carro.js
@@ -6,10 +6,33 @@ let cerrarCarrito = document.querySelector('.quitar-card')
 
 abrirCarrito.onclick = () => {
     Carrito.classList.add("active");
-
 };
 
 cerrarCarrito.onclick = () => {
     Carrito.classList.remove("active");
-
 };
+
+
+// Carrito Funcion
+
+if (document.readyState == "loading"){
+    document.addEventListener("DOMContentLoaded", ready);
+
+} else {
+    ready();
+}
+
+//Funciones quitar producto
+function ready(){
+    var quitarCardButon = document.getElementsByClassName('quitar-producto')
+    console.log(quitarCardButon)
+    for (var i = 0; i< quitarCardButon.length; i++){
+        var boton = quitarCardButon[i]
+        boton.addEventListener('click', quitarCardProducto)
+    }
+}
+
+function quitarCardProducto(event){
+    var clickBoton = event.target
+    clickBoton.parentElement.remove();
+}
